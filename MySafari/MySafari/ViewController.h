722d2872b8b5a15ebd834c71753933a1c333335c
沈新满 <shenxinman@shenxinmandemacbook-air.local>
2019-03-22 11:41:58 +0800

    two

--- MySafari/MySafari/ViewController.h
+++ MySafari/MySafari/ViewController.h
@@ -4,12 +4,12 @@
 //
 //  Created by 沈新满 on 2019/3/22.
 //  Copyright © 2019年 Sam. All rights reserved.
-//
+// 用于提供给外界一个加载网页地址的接口
 
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
 
-
+-(void)loadURL:(NSString*)urlStr;
 @end
 
